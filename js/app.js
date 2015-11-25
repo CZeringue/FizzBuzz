2da335d530c1e38db0efb1e6c46107eeb73493e2
CZeringue <cczeringue@icloud.com>
2015-11-24 19:30:31 -0800

    finished fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
-var x = prompt("Please give me a number.")
-var y = parseInt(x,10);
+
 
 
 $(document).ready(function () {
+var x = prompt("Please give me a number.")
+var y = parseInt(x,10);
+
 if (typeof y==='number' && (y%1)===0) {	
 	//else check to see if divisible by 3
 	//else check to divisible by 5
 	//else print
 	while (y > 0) { 
 	if (y%3===0 && y%5===0 && y!= 0) { //check to see if divisible by 3 AND 5
-		console.log("FizzBuzz");
+		$('body').append("<br />FizzBuzz");
 	}
 	else if (y%3===0) {
-		console.log("Fizz");
+		$('body').append("<br />Fizz");
 	}
 	else if (y%5===0) {
-		console.log("Buzz");
+			$('body').append("<br />Buzz");
 	}
 	else {
-		console.log(y)
+		$('body').append("<br /> ",y);
 	}
 	y--;
 }
