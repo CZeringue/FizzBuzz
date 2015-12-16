8e4ec14d017081c98b9d1c8401412dfb34cce951
CZeringue <cczeringue@icloud.com>
2015-12-15 20:49:30 -0800

    fixed order of answers

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
+$(document).ready(function() {
+    FizzBuzz(parseInt(prompt("Please give me a number."),10));
+});
 
-
-
-$(document).ready(function fizz () {
-var x = prompt("Please give me a number.")
-var y = parseInt(x,10);
-
-if (typeof y==='number' && (y%1)===0) {	
-	//else check to see if divisible by 3
-	//else check to divisible by 5
-	//else print
-	while (y > 0) { 
-	if (y%3===0 && y%5===0 && y!= 0) { //check to see if divisible by 3 AND 5
-		$('body').append("<br />FizzBuzz");
-	}
-	else if (y%3===0) {
-		$('body').append("<br />Fizz");
-	}
-	else if (y%5===0) {
-			$('body').append("<br />Buzz");
-	}
-	else {
-		$('body').append("<br /> ",y);
-	}
-	y--;
+function FizzBuzz (y) {
+    if (typeof y==='number' && (y%1)===0) { 
+    //else check to see if divisible by 3
+    //else check to divisible by 5
+    //else print
+    
+    for (i=1; i <= y; i++) { 
+    if (i%3===0 && i%5===0 && i!= 0) { //check to see if divisible by 3 AND 5
+        $('body').append("<br />FizzBuzz");
+    }
+    else if (i%3===0) {
+        $('body').append("<br />Fizz");
+    }
+    else if (i%5===0) {
+            $('body').append("<br />Buzz");
+    }
+    else {
+        $('body').append("<br /> ",i);
+    }
+   
 }
 }
 else {
-	alert("Try again.");
+    alert("Try again.");
 }
-
-
-});
+    
+    
+    }
